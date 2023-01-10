bc7fbd20b3d1159f3663e5120465cff5cbad6326
fajaryumna <fajaryumna2772@gmail.com>
2023-01-10 08:11:40 +0700

    Update routing

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('user.mainpage');
+});
+
+
+Route::get('/peminjamanpage', function () {
+    return view('user.peminjamanpage');
+});
+
+Route::get('/invoicepage', function () {
+    return view('user.invoicepage');
 });
