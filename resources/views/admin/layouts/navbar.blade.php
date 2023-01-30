fb0d45ccd508cceb435bb3bc914ae97fe2de0cbe
Abdullah Faqih Al Mubarak <82459624+fikiham@users.noreply.github.com>
2023-01-30 09:21:30 +0700

    logout admin

--- resources/views/admin/layouts/navbar.blade.php
+++ resources/views/admin/layouts/navbar.blade.php
@@ -2,12 +2,12 @@
 <div style="display:flex; justify-content: space-between; align-items: center;
      padding:0 30px; height:70px; background-color: #D98E04;">
     <img src="{{ URL::to('/images/logo-kejati.png') }}" width="17%" alt="Logo Perpustakaan Kejati">
-    <form action="{{ route('logout') }}" method="post">
+    <form id="logout-form" action="{{ route('logout') }}" method="post">
         @csrf
-        <a type="submit" class="dropdown-item" data-toggle="modal" data-target="#logoutModal">
+        <button type="submit" class="dropdown-item" data-toggle="modal" data-target="#logoutModal">
             <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400" style="color: black;"></i>
             Logout
-        </a>
+        </button>
     </form>
 
 </div>
